503371bf366dfc80825cd8e392fba7ae2b996628
hh <11@qq.com>
2020-12-29 15:23:25 +0800

    1229-zgg-搭建项目

---
+++ src/utils/http.js
@@ -0,0 +1,72 @@
+import axios from "axios"
+import qs from "qs"
+import Vue from "vue"
+import {erroralert} from "./alert"
+//开发环境使用 8080
+let baseUrl="/api"
+Vue.prototype.$pre="http://localhost:3000"
+
+//生产环境使用 打包
+// let baseUrl=""
+// Vue.prototype.$pre=""
+
+
+//响应拦截
+axios.interceptors.response.use(res=>{
+    console.group("本次请求地址是："+res.config.url)
+    console.log(res);
+    console.groupEnd()
+    //18.统一处理失败
+    if(res.data.code!==200){
+        erroralert(res.data.msg)
+    }
+    return res
+})
+
+/************菜单管理 start**************************/
+//7.添加
+export const reqMenuAdd=(user)=>{
+    return axios({
+        url:baseUrl+"/api/menuadd",
+        method:"post",
+        data:qs.stringify(user)
+    })
+}
+
+
+//19.列表
+export let reqMenulist=()=>{
+    return axios({
+        url:baseUrl+"/api/menulist",
+        params:{
+            istree:true
+        }
+    })
+}
+
+//30.删除
+export let reqMenuDel=(obj)=>{
+    return axios({
+        url:baseUrl+"/api/menudelete",
+        method:"post",
+        data:qs.stringify(obj)
+    })
+}
+
+//38.一条数据
+export let reqMenuDetail=(obj)=>{
+    return axios({
+        url:baseUrl+"/api/menuinfo",
+        params:obj
+    })
+}
+
+//39.修改接口
+export let reqMenuUpdate=(user)=>{
+    return axios({
+        url:baseUrl+"/api/menuedit",
+        method:"post",
+        data:qs.stringify(user)
+    })
+}
+/************菜单管理 end**************************/
